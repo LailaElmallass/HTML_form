61e331faeafca61e5d8f038bb51cca1a77fe1c28
Karima Benihda <karimabenihda@gmail.com>
2025-03-17 15:40:50 +0000

    changement du fichier main.js

--- main.js
+++ main.js
@@ -2,5 +2,8 @@ function refresh(){
     location.reload();
 }
 
-document.writeln("bro i'm practicing the gitflow
-    ,btw i'liked the style thank you")
+document.writeln("bro i'm practicing the gitflow,btw i'liked the style thank you")
+
+// j'ai fait des changement ici <3
+
+
